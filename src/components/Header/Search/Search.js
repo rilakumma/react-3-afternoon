d9b07ca38e26faaf2490caa478498675874ed375
Madison Walmsley <madi@Madisons-MacBook-Pro.local>
2018-12-21 11:51:31 -0700

    turn in

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -7,12 +7,28 @@ import SearchIcon from 'react-icons/lib/md/search';
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor(){
+    super();
+    this.state = {
+      text: ''
+    }
+  }
+
+
+  searchPost() {
+    const { text } = this.state;
+    const { searchPostFn } = this.props;
+    
+    searchPostFn(text);
+    this.setState({ text: '' })
+  }
+
   render() {
     return (
       <section className="Search__parent">
 
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input placeholder="Search Your Feed" onChange={e=> this.searchPost(e.target.value)} />
 
           <SearchIcon id="Search__icon" />
         </div>
